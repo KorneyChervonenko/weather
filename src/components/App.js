2b2ff763d2bd4cb724b31fb1af6c5d0dc2c51d44
Korney Chervonenko <pokryshkinkorney@gmail.com>
2024-12-15 22:41:12 +0200

    useGeolocation

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,40 @@
+import { useEffect } from 'react';
+import { useGeolocation } from './useGeolocation';
+
 import './App.scss';
 
-function App() {
-	return <div className="App">React App placeholder</div>;
-}
+export default function App() {
+	const {
+		isLoading,
+		position: { lat, lng },
+		error,
+		getPosition,
+	} = useGeolocation();
+	// console.log(lat, lng);
 
-export default App;
+	// useEffect(function () {
+	// 	console.clear();
+	// 	getPosition();
+	// }, []);
+
+	return (
+		<div className="App">
+			{/* <button onClick={getPosition} disabled={isLoading}>
+				Get my position
+			</button>
+			{error && <p>{error}</p>}
+			{!isLoading && !error && lat && lng && (
+				<p>
+					Your GPS position:{' '}
+					<a
+						target="_blank"
+						rel="noreferrer"
+						href={`https://www.openstreetmap.org/#map=16/${lat}/${lng}`}
+					>
+						{lat}, {lng}
+					</a>
+				</p>
+			)} */}
+		</div>
+	);
+}
